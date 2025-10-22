a3a4c9e1f652e85ae773564a4b4110f1572fc494
mateus-ely <mateus.elyc@gmail.com>
2025-10-22 15:47:18 -0300

    fix router 2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio_react_vite_deploy"
+  base: "/portfolio_react_vite_deploy/"
 });
